a31dade41e40f50b4629f99ec70fd1747eee0f77
eunsik <s@typer.kr>
2024-01-26 10:49:22 +0900

    슬라이드 적용

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,8 @@ import * as React from "react"
 import "../assets/css/page.main.scss"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css"
 
 const IndexPage = () => {
   React.useEffect(() => {
@@ -15,56 +17,91 @@ const IndexPage = () => {
   }, [])
 
   async function handleResize() {
-    const itemSize = window.outerHeight / 3
-    gsap.from(".white-bars > li:first-of-type", {
-      width: "50%",
-    })
+    const isMobile = window.innerWidth < 768
+    if (!isMobile) {
+      const itemSize = window.innerHeight / 3
+      gsap.to(".white-bars > li:first-of-type", {
+        duration: 1,
+        width: "50%",
+        x: 0,
+        y: 0,
+        ease: Power2.easeOut,
+      })
 
-    gsap.from(".white-bars > li:nth-of-type(2)", {
-      x: -itemSize,
-      width: "50%",
-    })
-    gsap.from(".white-bars > li:nth-of-type(3)", {
-      x: -itemSize * 2,
-      width: "50%",
+      gsap.to(".white-bars > li:nth-of-type(2)", {
+        duration: 0.9,
+        x: -itemSize,
+        y: 0,
+        width: "50%",
+        ease: Power2.easeOut,
+        delay: 0.1,
+      })
+
+      gsap.to(".white-bars > li:nth-of-type(3)", {
+        duration: 0.8,
+        x: -itemSize * 2,
+        y: 0,
+        width: "50%",
+        ease: Power2.easeOut,
+        delay: 0.2,
+      })
+    } else {
+      const itemSize = window.innerWidth / 3
+      gsap.to(".white-bars > li:first-of-type", {
+        duration: 1,
+        width: "100%",
+        height: "50%",
+        ease: Power2.easeOut,
+      })
+
+      gsap.to(".white-bars > li:nth-of-type(2)", {
+        duration: 0.9,
+        y: -itemSize,
+        x: 0,
+        width: "100%",
+        height: "50%",
+        ease: Power2.easeOut,
+        delay: 0.1,
+      })
+
+      gsap.to(".white-bars > li:nth-of-type(3)", {
+        duration: 0.8,
+        x: 0,
+        y: -itemSize * 2,
+        width: "100%",
+        height: "50%",
+        ease: Power2.easeOut,
+        delay: 0.2,
+      })
+    }
+
+    gsap.to(".copyright", {
+      color: "white",
+      // ease: Power2.easeOut,
     })
   }
 
   async function init() {
-    const itemSize = window.innerHeight / 3
-    gsap.to(".white-bars > li:first-of-type", {
-      duration: 1,
-      // x: 180,
-      width: "50%",
-      ease: Power2.easeOut,
-    })
+    const isMobile = window.innerWidth < 768
+    handleResize()
 
-    gsap.to(".white-bars > li:nth-of-type(2)", {
-      duration: 0.9,
-      x: -itemSize,
-      width: "50%",
-      ease: Power2.easeOut,
-      delay: 0.1,
-    })
-
-    gsap.to(".white-bars > li:nth-of-type(3)", {
+    gsap.to(".black-bg", {
+      alpha: 0,
       duration: 0.8,
-      x: -itemSize * 2,
-      width: "50%",
-      ease: Power2.easeOut,
-      delay: 0.2,
+      ease: Power2.easeInOut,
+      delay: 0.6,
     })
-
-    // gsap.to(".bg-overlay > .black-bg", {
-    //   alpha: 0,
-    //   duration: 0.8,
-    //   ease: Power2.easeInOut,
-    //   delay: 0.6,
-    // })
 
     setTimeout(() => {
-      gsap.to(".toggle-menu-btn .bar", {
-        stroke: "white",
+      if (!isMobile) {
+        gsap.to(".toggle-menu-btn .bar", {
+          stroke: "white",
+          // ease: Power2.easeOut,
+        })
+      }
+
+      gsap.to(".copyright", {
+        color: "white",
         // ease: Power2.easeOut,
       })
     }, 50)
@@ -94,45 +131,44 @@ const IndexPage = () => {
           start: "top",
           onEnter: () => {
             const target = index
-            console.log("enter:" + target)
-            document
-              .querySelectorAll(".bg-container > div")
-              .forEach((el, index) => {
-                if (index == target) {
-                  el.classList.add("active")
-                } else {
-                  el.classList.remove("active")
-                }
-              })
+            // document
+            //   .querySelectorAll(".bg-container > div")
+            //   .forEach((el, index) => {
+            //     if (index == target) {
+            //       el.classList.add("active")
+            //     } else {
+            //       el.classList.remove("active")
+            //     }
+            //   })
 
-            document.querySelectorAll(".txt-list > li").forEach((el, index) => {
-              if (index == target) {
-                el.classList.add("active")
-              } else {
-                el.classList.remove("active")
-              }
-            })
+            // document.querySelectorAll(".txt-list > li").forEach((el, index) => {
+            //   if (index == target) {
+            //     el.classList.add("active")
+            //   } else {
+            //     el.classList.remove("active")
+            //   }
+            // })
           },
           onLeaveBack: () => {
             const target = index
             console.log("leave:" + target)
-            document
-              .querySelectorAll(".bg-container > div")
-              .forEach((el, index) => {
-                if (index == target) {
-                  el.classList.add("active")
-                } else {
-                  el.classList.remove("active")
-                }
-              })
+            // document
+            //   .querySelectorAll(".bg-container > div")
+            //   .forEach((el, index) => {
+            //     if (index == target) {
+            //       el.classList.add("active")
+            //     } else {
+            //       el.classList.remove("active")
+            //     }
+            //   })
 
-            document.querySelectorAll(".txt-list > li").forEach((el, index) => {
-              if (index == target) {
-                el.classList.add("active")
-              } else {
-                el.classList.remove("active")
-              }
-            })
+            // document.querySelectorAll(".txt-list > li").forEach((el, index) => {
+            //   if (index == target) {
+            //     el.classList.add("active")
+            //   } else {
+            //     el.classList.remove("active")
+            //   }
+            // })
           },
         },
       })
@@ -142,83 +178,40 @@ const IndexPage = () => {
   return (
     <Layout pageName="index">
       <div className="main-page" data-lenis-prevent>
-        <div className="bg-container">
-          <div className="active">
-            <img src="/img/bg/1.webp" alt="" />
-          </div>
-          <div>
-            <img src="/img/bg/2.webp" alt="" />
-          </div>
-          <div>
-            <img src="/img/bg/3.webp" alt="" />
-          </div>
-          <div>
-            <img src="/img/bg/4.webp" alt="" />
-          </div>
-          <div>
-            <img src="/img/bg/5.webp" alt="" />
-          </div>
-          <div>
-            <img src="/img/bg/6.webp" alt="" />
-          </div>
-          <div>
-            <img src="/img/bg/7.webp" alt="" />
-          </div>
-          <div>
-            <img src="/img/bg/8.webp" alt="" />
-          </div>
-        </div>
         <div className="bg-overlay">
-          <div className="black-bg"></div>
-          <ul className="white-bars">
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-          <ul className="txt-list">
-            <li>
-              <span>WELCOME</span>
-              <span>ABOARD</span>
-            </li>
-            <li>
-              <span>On or Onto</span>
-              <span>Physical Interaction</span>
-            </li>
-            <li>
-              <span>On or Onto</span>
-              <span>Metaverse</span>
-            </li>
-            <li>
-              <span>On or Onto</span>
-              <span>User Experience Design</span>
-            </li>
-            <li>
-              <span>On or Onto</span>
-              <span>Interactive Design</span>
-            </li>
-            <li>
-              <span>On or Onto</span>
-              <span>Mixed Reality</span>
-            </li>
-            <li>
-              <span>On or Onto</span>
-              <span>Systems Engineering</span>
-            </li>
-            <li>
-              <span>On or Onto</span>
-              <span>AI Service Platform</span>
-            </li>
-          </ul>
-        </div>
-        <div className="scroll-container">
-          <div></div>
-          {/* <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div> */}
+          <Swiper
+            className="bg-swiper"
+            spaceBetween={0}
+            slidesPerView={1}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={swiper => console.log(swiper)}
+          >
+            <SwiperSlide>
+              <img src="/img/bg/1.webp" alt="" />
+            </SwiperSlide>
+            {/* <SwiperSlide>
+              <img src="/img/bg/2.webp" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="/img/bg/3.webp" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="/img/bg/4.webp" alt="" />
+            </SwiperSlide> */}
+            <div className="gradient-bg"></div>
+            <div className="black-bg"></div>
+            <ul className="white-bars">
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+            <ul className="txt-list">
+              <li>
+                <span>WELCOME</span>
+                <span>ABOARD</span>
+              </li>
+            </ul>
+          </Swiper>
         </div>
         <p className="copyright">©2023 ABOARD.</p>
       </div>
